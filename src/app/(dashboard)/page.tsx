e8442e7f818cb87bdcb13c537d4b95721ce47762
Tolu <t85491005@gmail.com>
2025-06-23 20:48:18 +0000

    the styling broke fix that

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -6,12 +6,12 @@ import { PersonalizedTasks } from "@/components/dashboard/personalized-tasks";
 export default function DashboardPage() {
   return (
     <div className="grid auto-rows-max items-start gap-4 md:gap-8">
-      <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <OverviewCards />
         <DailyRewards />
       </div>
-      <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="xl:col-span-2">
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-3">
+        <div className="lg:col-span-2">
             <ActivityHistory />
         </div>
         <div>
