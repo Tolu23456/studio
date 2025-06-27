41274207842f0124220b8facb781394e5d95a008
Tolu <t85491005@gmail.com>
2025-06-27 13:02:25 +0000

    It is not still working
    apiKey: "AIzaSyDS7HdjMRYUvk861pt0dbHCPXwR6w0

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getStorage, type FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDS7HdjMRYUvk861pt0dbHCPXwR6w0_Z0w",
+  authDomain: "adsener-app.firebaseapp.com",
+  projectId: "adsener-app",
+  storageBucket: "adsener-app.appspot.com",
+  messagingSenderId: "1005970824212",
+  appId: "1:1005970824212:web:324ede7c759bccd48cdb01",
+  measurementId: "G-25HG8VWZR0",
 };
 
 let app: FirebaseApp | null = null;
